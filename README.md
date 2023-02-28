2bd0d9d1b8b36c27c7cd2e9ad6015b4d01fdaa43
Vitor Gehlen <vitoorgehlen@gmail.com>
2023-02-27 21:51:37 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jogo da Velha
+
+## Um projeto de aprendizado.
---
+++ src/index.js
@@ -0,0 +1,7 @@
+/* Git Flow
+ Abrir o terminal (Crtl+J) e rodar esses comandos
+ ! Entre cada comando: "git status"
+ Depois "git add"
+
+
+*/
